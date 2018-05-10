2641b013fe430bd0ea9c093d1b0c722ca07c1416
Tim Richardson <tim@timrichardson.co>
2018-05-10 10:11:48 +0100

    Added Write and Import file capabilities

--- UsingFiles/ViewController.swift
+++ UsingFiles/ViewController.swift
@@ -7,13 +7,87 @@
 //
 
 import UIKit
+import MobileCoreServices
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
+    
+    @IBAction func writeFiles(_ sender: Any) {
         
+        let file = "\(UUID().uuidString).txt"
+        let contents = "Some text..."
         
+        let dir = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!
+        let fileURL = dir.appendingPathComponent(file)
         
+        do {
+            try contents.write(to: fileURL, atomically: false, encoding: .utf8)
+        }
+        catch {
+            print("Error: \(error)")
+        }
+    }
+    
+    @IBAction func importFiles(_ sender: Any) {
+        
+        let documentPicker = UIDocumentPickerViewController(documentTypes: [kUTTypePlainText as String], in: .import)
+        documentPicker.delegate = self
+        documentPicker.allowsMultipleSelection = false
+        present(documentPicker, animated: true, completion: nil)
     }
 }
+
+extension ViewController: UIDocumentPickerDelegate {
+    
+    func documentPicker(_ controller: UIDocumentPickerViewController, didPickDocumentsAt urls: [URL]) {
+        
+        guard let selectedFileURL = urls.first else {
+            return
+        }
+        
+        let dir = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!
+        let sandboxFileURL = dir.appendingPathComponent(selectedFileURL.lastPathComponent)
+        
+        if FileManager.default.fileExists(atPath: sandboxFileURL.path) {
+            print("Already exists! Do nothing")
+        }
+        else {
+            
+            do {
+                try FileManager.default.copyItem(at: selectedFileURL, to: sandboxFileURL)
+                
+                print("Copied file!")
+            }
+            catch {
+                print("Error: \(error)")
+            }
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
